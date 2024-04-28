318b41820f774e342957992ec1262e140d70e8c6
devklick <devklick@gmail.com>
2024-04-28 10:58:30 +0100

    feat: update list command shorthand to ls

--- src/GDMan.Cli/Options/ListOptions.cs
+++ src/GDMan.Cli/Options/ListOptions.cs
@@ -2,7 +2,7 @@ using GDMan.Cli.Attributes;
 
 namespace GDMan.Cli.Options;
 
-[Command("list", "l", "Lists the versions of Godot currently installed on the system")]
+[Command("list", "ls", "Lists the versions of Godot currently installed on the system")]
 public class ListOptions : BaseOptions
 {
 }
